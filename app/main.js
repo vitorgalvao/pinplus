9431cbe6fb524e46394264903fdec97478de85f2
Drew Nall <drew@callrail.com>
2019-12-30 01:10:45 -0500

    Add support for Brave Browser

--- app/main.js
+++ app/main.js
@@ -44,7 +44,7 @@ function getUrlTitle(pageUrl, pageTitle) {
     const frontmost_app_name = Application('System Events').applicationProcesses.where({ frontmost: true }).name()[0]
     const frontmost_app = Application(frontmost_app_name)
 
-    if (['Google Chrome','Google Chrome Canary','Chromium','Opera','Vivaldi'].indexOf(frontmost_app_name) > -1) {
+    if (['Google Chrome','Google Chrome Canary','Chromium','Opera','Vivaldi','Brave Browser'].indexOf(frontmost_app_name) > -1) {
       var current_tab_title = frontmost_app.windows[0].activeTab.name()
       var current_tab_url = frontmost_app.windows[0].activeTab.url()
     } else if (['Safari','Safari Technology Preview','Webkit'].indexOf(frontmost_app_name) > -1) {
